14fffdf04e6666118b00207eae9688f52dac8d09
ayushgajbhiye <hoonayush887@gmail.com>
2025-07-18 16:17:04 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+// External packages
 import 'package:flutter/material.dart';
-import 'package:metafit/presentation/auth/sign_up_screen.dart';
-import 'package:metafit/presentation/auth/login_screen.dart';
-import 'package:metafit/presentation/welcome/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+// Local project files
+import 'package:metafit/presentation/auth/login_screen.dart';
+import 'package:metafit/presentation/auth/sign_up_screen.dart';
+import 'package:metafit/presentation/home/bmi_input_screen.dart';
+import 'package:metafit/presentation/welcome/splash_screen.dart';
 import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp(
@@ -30,6 +35,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => const SplashScreen(),
         '/login': (context) => const LoginScreen(),
         '/signup': (context) => const SignUpScreen(title: 'metafit',),
+        '/bmi': (context) => const BmiInputScreen(),
       },
     );
   }
